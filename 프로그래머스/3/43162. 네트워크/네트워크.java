96a8407996427a581321764921ce5878703ce65b
한송이 <73270553+1Min-seo@users.noreply.github.com>
2025-05-09 11:36:40 +0900

    [level 3] Title: 네트워크, Time: 0.20 ms, Memory: 83.9 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -3,11 +3,11 @@ class Solution {
         boolean[] visited = new boolean[n];
         int cnt = 0;
         
-        for(int i = 0; i < computers.length; i++) {
-            if(!visited[i]){
+        for(int i = 0; i < n; i++) {
+            if(!visited[i]) {
                 dfs(i, computers, visited);
                 cnt++;
-            }      
+            }
         }
         return cnt;
     }
@@ -16,9 +16,10 @@ class Solution {
         visited[idx] = true;
         
         for(int i = 0; i < computers.length; i++) {
-            if(computers[idx][i] == 1 && !visited[i] && i != idx) {
+            if(idx != i && computers[idx][i] == 1 && !visited[i]) {
                 dfs(i, computers, visited);
             }
         }
+        return;
     }
 }
