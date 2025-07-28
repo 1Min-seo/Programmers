cc4266279f7de1377eced4aecb0d578277a17ebd
한송이 <73270553+1Min-seo@users.noreply.github.com>
2025-07-28 19:09:02 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 10.27 ms, Memory: 92.1 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
@@ -1,51 +1,47 @@
 import java.util.*;
 class Solution {
     public int solution(String str1, String str2) {
-        List<String> list1 = makeMultiSet(str1);
-        List<String> list2 = makeMultiSet(str2);
+        String s1 = str1.toLowerCase();
+        String s2 = str2.toLowerCase();
+        Map<String, Integer> hmS1 = new HashMap<>();
+        Map<String, Integer> hmS2 = new HashMap<>();
         
-        Map<String, Integer> hm1 = new HashMap<>();
-        Map<String, Integer> hm2 = new HashMap<>();
-        
-        for(String s : list1) {
-            hm1.put(s, hm1.getOrDefault(s, 0) + 1);
-        }
-        for(String s : list2) {
-             hm2.put(s, hm2.getOrDefault(s, 0) + 1);
-        }
-        
-        int intersection = 0;
-        int union = 0;
-        
-        Set<String> allKeys = new HashSet<>();
-        allKeys.addAll(hm1.keySet());
-        allKeys.addAll(hm2.keySet());
-        
-        for(String key : allKeys) {
-            int count1 = hm1.getOrDefault(key, 0);
-            int count2 = hm2.getOrDefault(key, 0);
+        for(int i = 0; i < str1.length() - 1; i++) {
+            char a = s1.charAt(i);
+            char b = s1.charAt(i + 1);
             
-            intersection += Math.min(count1, count2);
-            union += Math.max(count1, count2);
-        }
-        
-        if (union == 0) return 65536;
-        return (int)(((double) intersection / union) * 65536);
+            if (!Character.isLetter(a) || !Character.isLetter(b)) continue;
 
-        
-    }
-    
-    private List<String> makeMultiSet(String str) {
-        List<String> result = new ArrayList<>();
-        str = str.toLowerCase();
-        for(int i = 0; i < str.length() - 1; i++) {
-            char a = str.charAt(i);
-            char b = str.charAt(i + 1);
-            
-            if(Character.isLetter(a) && Character.isLetter(b)) {
-                result.add("" + a + b);
-            }
+            String s = "" + a + b;
+            hmS1.put(s, hmS1.getOrDefault(s, 0) + 1);
         }
-        return result;
+        
+        for(int i = 0; i < str2.length() - 1; i++) {
+            char a = s2.charAt(i);
+            char b = s2.charAt(i + 1);
+            
+            if (!Character.isLetter(a) || !Character.isLetter(b)) continue;
+
+            String s = "" + a + b;
+            hmS2.put(s, hmS2.getOrDefault(s, 0) + 1);
+        }
+        
+        Set<String> hsUnion = new HashSet<>();
+        hsUnion.addAll(hmS1.keySet());
+        hsUnion.addAll(hmS2.keySet());
+        
+        int inter = 0;
+        int union = 0;
+        for(String key : hsUnion) {
+            int cnt1 = hmS1.getOrDefault(key, 0);
+            int cnt2 = hmS2.getOrDefault(key, 0);
+            
+            inter += Math.min(cnt1, cnt2);
+            union += Math.max(cnt1, cnt2);
+        }
+        
+        if(union == 0) return 65536;
+        return (int) ((inter * 1.0 / union) * 65536);
+        
     }
 }
