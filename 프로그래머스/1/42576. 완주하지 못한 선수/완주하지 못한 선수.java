019c11ae9ecc4825599a5a3c74fee66ffe86de1a
한민서 <73270553+1Min-seo@users.noreply.github.com>
2025-09-03 01:27:36 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 118.42 ms, Memory: 98.5 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,19 +1,25 @@
 import java.util.*;
-
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        HashMap<String, Integer> hm = new HashMap<>();
+        Map<String, Integer> names = new HashMap<>();
+        StringBuilder sb = new StringBuilder();
         
-        for(String name : completion) {
-            hm.put(name, hm.getOrDefault(name, 0) + 1);
+        for(String parti : participant) {
+            names.put(parti, names.getOrDefault(parti, 0) + 1);
         }
         
-        for(String name : participant) {
-            if(!hm.containsKey(name) || hm.get(name) == 0) {
+        for(String com : completion) {
+            if(names.containsKey(com)) {
+                names.put(com, names.get(com) - 1);
+            }
+        }
+        
+        for(String name : names.keySet()) {
+            if(names.get(name) > 0) {
                 return name;
             }
-            hm.put(name, hm.get(name) - 1);
         }
+        
         return "";
     }
 }
