83f0f16b50d502faaee6e3a290aaacafb10e1dbf
한민서 <73270553+1Min-seo@users.noreply.github.com>
2025-09-03 02:27:50 +0900

    [level 2] Title: 의상, Time: 0.14 ms, Memory: 110 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,18 +1,24 @@
 import java.util.*;
 class Solution {
     public int solution(String[][] clothes) {
-        Map<String, Integer> hm = new HashMap<>();
-        int answer = 1;
+        Map<String, Integer> clotheMap = new HashMap<>();
         
-        for(String[] cloth : clothes) {
-            String type = cloth[1];
-            hm.put(type, hm.getOrDefault(type, 0) + 1);
+        for(String[] clothe : clothes) {
+            String type = clothe[1];
+            clotheMap.put(type, clotheMap.getOrDefault(type, 0) + 1);
         }
         
-        for(int cnt : hm.values()) {
-            answer *= (cnt + 1);
+        int answer = 1;
+        for(int count : clotheMap.values()) {
+            answer *= (count + 1);
         }
         
         return answer - 1;
     }
 }
+
+/**
+    headgear: yellow, green
+    eyewear : blue
+
+*/
