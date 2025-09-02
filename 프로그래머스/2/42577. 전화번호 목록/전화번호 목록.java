765e00065045fd9b126033de875629cfec707665
한민서 <73270553+1Min-seo@users.noreply.github.com>
2025-09-03 02:00:37 +0900

    [level 2] Title: 전화번호 목록, Time: 286.10 ms, Memory: 249 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,11 +1,17 @@
 import java.util.*;
 class Solution {
     public boolean solution(String[] phone_book) {
-        Arrays.sort(phone_book);
+        Set<String> phoneSet = new HashSet<>();
+        for(String phone : phone_book) {
+            phoneSet.add(phone);
+        }
         
-        for(int i = 0; i < phone_book.length - 1; i++){
-            if(phone_book[i + 1].startsWith(phone_book[i])){
-                return false;
+        for(String phone : phone_book) {
+            for(int i = 1; i < phone.length(); i++) {
+                String prefix = phone.substring(0, i);
+                
+                if(phoneSet.contains(prefix)) return false;
+                
             }
         }
         return true;
