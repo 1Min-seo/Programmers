e0f8433e4f686046d3f8090b8ef629e01e3d9d41
한민서 <73270553+1Min-seo@users.noreply.github.com>
2025-09-05 00:40:38 +0900

    [level 2] Title: 기능개발, Time: 3.17 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,28 +1,28 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        List<Integer> result = new ArrayList<>();
         int n = progresses.length;
-        int[] days = new int[n];
+        int[] remain = new int[n];
         
-        for(int i = 0; i < n; i++){
-            days[i] = (int)Math.ceil((100.0 - progresses[i]) / speeds[i]);
+        for(int i = 0; i < n; i++) {
+           remain[i] = (int)Math.ceil((100 - progresses[i]) / (double)speeds[i]); 
         }
         
-        int deployDay = days[0];
-        int cnt = 1;
+        int count = 1;
+        int target = remain[0];
+        ArrayList<Integer> result = new ArrayList<>();
         
-        for(int i = 1; i < n; i++){
-            if(deployDay >= days[i]) cnt += 1;
-            else{
-                result.add(cnt);
-                deployDay = days[i];
-                cnt = 1;
+        for(int i = 1; i < n; i++) {
+            if(target >=remain[i]) {
+                count++;
+            }else{
+                result.add(count);
+                count = 1;
+                target = remain[i];
             }
+            
         }
-        
-        result.add(cnt);
+        result.add(count);
         return result.stream().mapToInt(i -> i).toArray();
     }
 }
-
