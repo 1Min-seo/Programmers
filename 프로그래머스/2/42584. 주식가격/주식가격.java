93d04f317256464e9c071183d4b993dcf9b4bc11
한민서 <73270553+1Min-seo@users.noreply.github.com>
2025-09-05 02:24:54 +0900

    [level 2] Title: 주식가격, Time: 17.52 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,18 +1,18 @@
 class Solution {
     public int[] solution(int[] prices) {
-        int[] answer = new int[prices.length];
-
-        for (int i = 0; i < prices.length; i++) {
-            int seconds = 0;
-            for (int j = i + 1; j < prices.length; j++) {
-                seconds++;
-                if (prices[j] < prices[i]) {
+        
+        int[] result = new int[prices.length];
+        for(int i = 0; i < prices.length; i++) {
+            int target = prices[i];
+            int count = 0;
+            for(int j = i + 1; j < prices.length; j++) {
+                count++;
+                if(target > prices[j]) {
                     break;
-                }
+                } 
             }
-            answer[i] = seconds;
+            result[i] = count;
         }
-
-        return answer;
+        return result;
     }
 }
