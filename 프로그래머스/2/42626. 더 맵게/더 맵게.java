61df620f59b2e8a6963c55cc6067e3e51cb45b1e
한민서 <73270553+1Min-seo@users.noreply.github.com>
2025-09-11 22:29:19 +0900

    [level 2] Title: 더 맵게, Time: 1467.17 ms, Memory: 129 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -2,20 +2,24 @@ import java.util.*;
 class Solution {
     public int solution(int[] scoville, int K) {
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-        int result = 0;
-        for(int i = 0; i < scoville.length; i++){
-            pq.add(scoville[i]);
+        
+        for(int n : scoville) {
+            pq.offer(n);
         }
         
-        while(pq.peek() < K) {
-            if(pq.size() < 2) return -1;
-            int food1 = pq.poll();
-            int food2 = pq.poll();
+        int count = 0;
+        int sum = 0;
+        while(pq.size() > 1 && pq.peek() < K) {
+            int first = pq.poll();
+            int second = pq.poll();
             
-            int makeFood = food1 + (food2 * 2);
-            pq.add(makeFood);
-            result++;
+            sum = first + (second * 2);
+            pq.offer(sum);
+            count += 1;  
         }
-        return result;
+        
+        if(pq.peek() >= K) return count;
+        return -1;
+        
     }
 }
