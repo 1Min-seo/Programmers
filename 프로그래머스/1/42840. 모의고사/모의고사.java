abd26ef682244cc16ad144052403dd58af432c06
한민서 <73270553+1Min-seo@users.noreply.github.com>
2025-09-15 01:55:33 +0900

    [level 1] Title: 모의고사, Time: 3.93 ms, Memory: 81.1 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,30 +1,25 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] answers) {
-        int[] a = {1, 2, 3, 4, 5};
-        int[] b = {2 ,1, 2, 3, 2, 4, 2, 5};
-        int[] c = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        int student1[] = {1,2,3,4,5};
+        int student2[] = {2,1,2,3,2,4,2,5};
+        int student3[] = {3,3,1,1,2,2,4,4,5,5};
         
-        int aCnt = 0, bCnt = 0, cCnt = 0;
+        int[] score = new int[3];
         
-        for(int i = 0; i < answers.length ; i++) {
-	        if(answers[i] == a[i % a.length]) aCnt++;
-	        if(answers[i] == b[i % b.length]) bCnt++;
-	        if(answers[i] == c[i % c.length]) cCnt++;
+        for(int i = 0; i < answers.length; i++) {
+            if(answers[i] == student1[i % student1.length]) score[0]++;
+            if(answers[i] == student2[i % student2.length]) score[1]++;
+            if(answers[i] == student3[i % student3.length]) score[2]++;
         }
         
-        int result = Math.max(Math.max(aCnt, bCnt), cCnt);
+        int max = Math.max(score[0], Math.max(score[1], score[2]));
         
-        ArrayList<Integer> answer= new ArrayList<>();
-        if(result == aCnt) {
-            answer.add(1);
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0; i < 3; i++) {
+            if(score[i] == max) list.add(i + 1);
         }
-        if(result == bCnt) {
-            answer.add(2);
-        }
-        if(result == cCnt) {
-            answer.add(3);
-        }
-        return answer.stream().mapToInt(i->i).toArray();
+        
+        return list.stream().mapToInt(i -> i).toArray();
     }
 }
