b8e73832bc73b008bf3c9ea8fe058552d2f81d18
한민서 <73270553+1Min-seo@users.noreply.github.com>
2025-09-17 00:23:14 +0900

    [level 2] Title: [1차] 캐시, Time: 35.25 ms, Memory: 113 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -1,29 +1,27 @@
 import java.util.*;
 class Solution {
     public int solution(int cacheSize, String[] cities) {
-        if (cacheSize == 0) return cities.length * 5;
-        
-        Deque<String> cache = new LinkedList<>();
+        if(cacheSize == 0) return cities.length * 5;
         int time = 0;
         
+        Deque<String> dq = new LinkedList<>();
         for(String city : cities) {
-            String c = city.toLowerCase();
-            
-            if(cache.contains(c)) {
-                cache.remove(c);
-                cache.addLast(c);
+            city = city.toLowerCase();
+            if(dq.contains(city)) {
+                if(dq.size() == cacheSize) {
+                    dq.remove(city);
+                }
+                dq.addLast(city);
                 time += 1;
-            }else if(cache.size() == cacheSize) {
-                    cache.pollFirst();
-                    cache.addLast(c);
-                    time += 5;
-                
             }else{
-            cache.addLast(c);
-            time += 5;
+                if(dq.size() == cacheSize) {
+                    dq.pollFirst();
+                }
+                dq.addLast(city);
+                time += 5;
+            } 
         }
-        }
-        return time;
         
+        return time;
     }
 }
