906efb1266084fe35ff00d6e80b4ad72168a5166
한민서 <73270553+1Min-seo@users.noreply.github.com>
2025-09-16 22:16:46 +0900

    [level 2] Title: 괄호 회전하기, Time: 56.55 ms, Memory: 93.2 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -1,31 +1,32 @@
 import java.util.*;
 class Solution {
     public int solution(String s) {
-        int cnt = 0;
-        int len = s.length();
-        
-        for(int i = 0; i < len; i++) {
-            String rotated = s.substring(i) + s.substring(0, i);
-            if(isValid(rotated)) {
-                cnt++;
+        int n = s.length();
+        int count = 0;
+        for(int i = 0; i < n; i++) {
+            String newStr = s.substring(i) + s.substring(0, i);
+            
+            if(isValid(newStr)) {
+                count++;
             }
         }
-        return cnt;
+        
+        return count;
     }
     
-    private boolean isValid(String str) {
+    private boolean isValid(String s) {
         Stack<Character> st = new Stack<>();
-        
-        for(char c : str.toCharArray()) {
-            if(c == '(' || c == '{' || c == '[') {
+        for(char c : s.toCharArray()) {
+            if(c == '[' || c == '(' || c == '{') {
                 st.push(c);
             }else {
                 if(st.isEmpty()) return false;
-                
-                char top = st.pop();
-                if(c == ')' && top != '(') return false;
-                if(c == '}' && top != '{') return false;
-                if(c == ']' && top != '[') return false;
+                char prev = st.pop();
+                if((c == ']' && prev != '[') ||
+                  (c == ')' && prev != '(') ||
+                  (c == '}' && prev != '{')) {
+                      return false;
+                  }
             }
         }
         
