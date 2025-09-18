ad066d4d5aba13785915a096113bb07765008b81
한민서 <73270553+1Min-seo@users.noreply.github.com>
2025-09-18 20:02:31 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 12.98 ms, Memory: 69.4 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
@@ -1,47 +1,56 @@
 import java.util.*;
 class Solution {
     public int solution(String str1, String str2) {
-        String s1 = str1.toLowerCase();
-        String s2 = str2.toLowerCase();
-        Map<String, Integer> hmS1 = new HashMap<>();
-        Map<String, Integer> hmS2 = new HashMap<>();
+        str1 = str1.toLowerCase();
+        str2 = str2.toLowerCase();
+        List<String> list1 = makeTwoSplit(str1);
+        List<String> list2 = makeTwoSplit(str2);
         
-        for(int i = 0; i < str1.length() - 1; i++) {
-            char a = s1.charAt(i);
-            char b = s1.charAt(i + 1);
-            
-            if (!Character.isLetter(a) || !Character.isLetter(b)) continue;
-
-            String s = "" + a + b;
-            hmS1.put(s, hmS1.getOrDefault(s, 0) + 1);
-        }
+        Map<String, Integer> map1 = makeMap(list1);
+        Map<String, Integer> map2 = makeMap(list2);
         
-        for(int i = 0; i < str2.length() - 1; i++) {
-            char a = s2.charAt(i);
-            char b = s2.charAt(i + 1);
-            
-            if (!Character.isLetter(a) || !Character.isLetter(b)) continue;
-
-            String s = "" + a + b;
-            hmS2.put(s, hmS2.getOrDefault(s, 0) + 1);
-        }
+        Set<String> allKey = new HashSet<>();
+        allKey.addAll(map1.keySet());
+        allKey.addAll(map2.keySet());
         
-        Set<String> hsUnion = new HashSet<>();
-        hsUnion.addAll(hmS1.keySet());
-        hsUnion.addAll(hmS2.keySet());
-        
-        int inter = 0;
         int union = 0;
-        for(String key : hsUnion) {
-            int cnt1 = hmS1.getOrDefault(key, 0);
-            int cnt2 = hmS2.getOrDefault(key, 0);
+        int intersect = 0;
+        
+        for(String key : allKey) {
+            int count1 = map1.getOrDefault(key, 0);
+            int count2 = map2.getOrDefault(key, 0);
             
-            inter += Math.min(cnt1, cnt2);
-            union += Math.max(cnt1, cnt2);
+            union += Math.max(count1, count2);
+            intersect += Math.min(count1, count2);
         }
         
-        if(union == 0) return 65536;
-        return (int) ((inter * 1.0 / union) * 65536);
+        double jaccard;
+        if(union == 0) jaccard = 1.0;
+        else jaccard = (double)intersect / union;
         
+        return (int) (jaccard * 65536);
+    }
+    
+    private List<String> makeTwoSplit(String str) {
+        List<String> result = new ArrayList<>();
+        
+        for(int i = 0; i < str.length() - 1; i++) {
+            char a = str.charAt(i);
+            char b = str.charAt(i + 1);
+            
+            if(Character.isLetter(a) && Character.isLetter(b)) {
+                result.add("" + a + b);
+            }
+        }
+        return result;
+    }
+    
+    private Map<String, Integer> makeMap(List<String> str) {
+        Map<String, Integer> result = new HashMap<>();
+        for(String s : str) {
+            result.put(s, result.getOrDefault(s, 0) + 1);
+        }
+        
+        return result;
     }
 }
