e56138da1bd4a9ef9f84ea9096e14a56ca3c8783
한민서 <73270553+1Min-seo@users.noreply.github.com>
2025-09-19 01:29:06 +0900

    [level 2] Title: 스킬트리, Time: 0.26 ms, Memory: 88.7 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -1,21 +1,19 @@
+import java.util.*;
+
 class Solution {
     public int solution(String skill, String[] skill_trees) {
+        Set<Character> skillSet = new HashSet<>();
+        for(char c : skill.toCharArray()) skillSet.add(c);
+
         int count = 0;
-        
         for(String tree : skill_trees) {
-            StringBuilder sb = new StringBuilder();
-            
-            for(int i = 0; i < tree.length(); i++) {
-                char c = tree.charAt(i);
-                if(skill.contains(String.valueOf(c))) {
-                    sb.append(c);
-                }
+            StringBuilder filtered = new StringBuilder();
+            for(char c : tree.toCharArray()) {
+                if(skillSet.contains(c)) filtered.append(c);
             }
-            
-            if(skill.startsWith(sb.toString())) {
-                count++;
-            }
+            if(skill.startsWith(filtered.toString())) count++;
         }
+
         return count;
     }
 }
